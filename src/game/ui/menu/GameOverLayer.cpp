e9b8dc69ba08455feb588802386c7a797632408b
Daniel <dbcongard@gmail.com>
2023-08-26 14:26:58 +0200

    Show game over menu after spacecraft destruction

--- src/game/ui/menu/GameOverLayer.cpp
+++ src/game/ui/menu/GameOverLayer.cpp
@@ -1,17 +1,27 @@
 #include "GameOverLayer.h"
 #include "game/ui/GameUIScene.h"
+#include "game/GameScene.h"
 
 namespace UI {
 GameOverLayer::GameOverLayer(GameUIScene *scene)
     : MenuLayer(scene, "ui/GameOver.xml")
 {
     CloseAction closeAction = [scene]() {
-//        auto game = Game::getInstance(); TODO
-//        game->reset();
+        scene->parentGameScene()->triggerReset();
         scene->showLayerInsteadOf(scene->start, scene->game);
     };
 
     setCloseWidget(getContainer(), closeAction);
     setCloseWidget(getContainer()->findChild<Widget*>("label"), closeAction);
+
+    auto gameScene = scene->parentGameScene();
+
+    gameScene->getMechanics().addOnDestroyedListener([this] {
+        show();
+    });
+
+    gameScene->addOnResetCompletedListener([this] {
+        // TODO
+    });
 }
 } // UI
